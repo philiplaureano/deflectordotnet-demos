ee5f3634729e5a52ab3a506316f8bbb6d4de41f5
Philip Laureano <philip.laureano@gmail.com>
2016-03-28 20:11:32 +1100

    Added the demo for intercepting instance methods

--- Deflector.Demos/Deflector.Demos/Program.cs
+++ Deflector.Demos/Deflector.Demos/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SampleLibrary;
 
 namespace Deflector.Demos
 {
@@ -10,6 +11,34 @@ namespace Deflector.Demos
     {
         static void Main(string[] args)
         {
+            var targetAssembly = typeof(SampleClassWithInstanceMethod).Assembly;
+
+            // Notice how modifying the sample assembly requires only one line of code
+            var modifiedAssembly = targetAssembly.AddInterceptionHooks();
+
+            // Replace the DoSomethingElse() method call with a different method call
+            Replace.Method<SampleClassThatCallsAnInstanceMethod>(c => c.DoSomethingElse())
+                .With(() =>
+                {
+                    Console.WriteLine("DoSomethingElse() method call intercepted");
+                });
+
+            var targetTypeName = nameof(SampleClassThatCallsAnInstanceMethod);
+
+            // Use the createInstance lambda for syntactic sugar
+            Func<string, dynamic> createInstance = typeName =>
+            {
+                var targetType = modifiedAssembly.GetTypes().First(t => t.Name == typeName);
+                return Activator.CreateInstance(targetType);
+            };
+
+            // Create the modified type instance
+            dynamic targetInstance = createInstance(targetTypeName);
+
+            targetInstance.DoSomething();
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
         }
     }
 }
